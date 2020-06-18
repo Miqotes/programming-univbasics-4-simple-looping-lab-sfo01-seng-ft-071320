9f2d306eab662b09d13bc71c20c0a07d8dbabca7
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-18 04:17:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,11 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  uptime = 0
+  while message[uptime] <= 5 do
+    puts message[uptime]
+    message += 1
+  
+end
+
+def loop_message_n_times(message, number)
